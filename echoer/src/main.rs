00d445e34702ace22a6f2a31f1e4cff6354ea485
akumar23 <kumar.aryan@gmail.com>
2022-12-20 22:37:01 -0800

    added custom help text with flag library

--- echoer/src/main.rs
+++ echoer/src/main.rs
@@ -1,3 +1,26 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("{:?}", std::env::args()); //prints command line arguments
+
+    let matches = App::new("echoer")
+    .version("0.1.0")
+    .author("Aryan Kumar")
+    .about("rust echo clone")
+    .arg(
+        Arg::with_name("text")
+        .value_name("TEXT")
+        .help("Input text")
+        .required(true)
+        .min_values(1),
+    )
+    .arg(
+        Arg::with_name("omit_newline")
+        .short("n")
+        .help("Do not print newline")
+        .takes_value(false),
+    )
+    .get_matches();
+
+    println!("{:#?", matches);
+
 }
